ab9cef7c91cdeddbc6ff3c03af00bfe9eae602ab
Blake <54608589+Blakeb57@users.noreply.github.com>
2020-01-24 17:12:19 -0500

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Blake Boehringer
+ *        Date: Jan/24/2020
+ * Description: Purpose of code is simply to say hello git when the code is executed.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
